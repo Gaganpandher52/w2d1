07f78f498c4eb529fde2a0f38896e81f64483539
Gaganpandher52 <gaganpandher52@gmail.com>
2019-05-06 18:16:03 +0000

    v2

---
+++ printInChunks.js
@@ -0,0 +1,12 @@
+function getAndPrintHTMLChunks () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+    console.log(requestOptions);
+    console.log("hello world")
+    /* Add your code here */
+  
+  }
+  getAndPrintHTMLChunks()
